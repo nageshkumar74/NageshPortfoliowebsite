ccc9e953dbc7aab488e017477c3d58b1e147edbc
nageshkumar74 <82663413+nageshkumar74@users.noreply.github.com>
2025-08-13 13:57:08 +0530

    1st upload

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Outfit, Ovo} from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
+});
+// hello
+const ovo = Ovo({
+  subsets: ["latin"], weight: ["400"]
+});
+
+
+
+export const metadata = {
+  title: "Portfolio - NageshkumarGunji",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
